36569b7020f52a8030fa10088153c519bc27bc1e
señorMesa <sergioperez.iecc@gmail.com>
2026-04-07 21:55:42 +0100

    feat: .env with variables for postgres login

---
+++ src/main/java/com/nosmoke/nexus_ai/NexusAiApplication.java
@@ -0,0 +1,13 @@
+package com.nosmoke.nexus_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusAiApplication.class, args);
+	}
+
+}
