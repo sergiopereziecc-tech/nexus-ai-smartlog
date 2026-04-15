3f0c6bf86063cff7aeae2705c7b1361f186c529e
señorMesa <sergioperez.iecc@gmail.com>
2026-04-15 22:14:36 +0100

    feat: create method Service Layer implemented using mapper class

--- src/main/java/com/nosmoke/nexus_ai/service/ErrorServiceImpl.java
+++ src/main/java/com/nosmoke/nexus_ai/service/ErrorServiceImpl.java
@@ -1,9 +1,13 @@
 package com.nosmoke.nexus_ai.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nosmoke.nexus_ai.dtos.ErrorRequest;
 import com.nosmoke.nexus_ai.dtos.ErrorResponse;
+import com.nosmoke.nexus_ai.mapper.ErrorMapper;
 import com.nosmoke.nexus_ai.model.ErrorLog;
 import com.nosmoke.nexus_ai.repository.ErrorLogRepository;
 
@@ -13,12 +17,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ErrorServiceImpl implements ErrorService{
     
-    final ErrorLogRepository errorLogRepository;
+    private final ErrorLogRepository errorLogRepository;
+    private final ErrorMapper errorMapper;
 
     @Override
     public ErrorResponse create(ErrorRequest errorRequest) {
         
-    }  
+        ErrorLog errorLog = errorMapper.toEntity(errorRequest);
+        errorLogRepository.save(errorLog);
+        return errorMapper.toResponse(errorLog);
+        
+    } 
+    
+    @Override
+    public Optional<ErrorLog> read(Long id) {
+        
+        return Optional.empty();
+    }
     
 
 }
