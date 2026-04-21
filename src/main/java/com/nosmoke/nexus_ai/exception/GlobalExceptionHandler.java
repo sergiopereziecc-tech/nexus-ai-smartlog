64db8e1f7f6ec2f367a3e44cc52092b88be5e3f9
señorMesa <sergioperez.iecc@gmail.com>
2026-04-21 14:48:38 +0100

    refactor: global excepcion handler class from String to DTO, use of WebRequest too

--- src/main/java/com/nosmoke/nexus_ai/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nosmoke/nexus_ai/exception/GlobalExceptionHandler.java
@@ -1,18 +1,38 @@
 package com.nosmoke.nexus_ai.exception;
 
+import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import com.nosmoke.nexus_ai.dtos.ApiError;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler{
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFound.class)
-    public ResponseEntity<String> handleResourceNotFound(ResourceNotFound ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFound ex, WebRequest request) {
+
+        ApiError apiError = new ApiError(LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
 
     }
-    
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleGlobalException(Exception ex, WebRequest request) {
+
+        ApiError apiError = new ApiError(LocalDateTime.now(),
+                "An internal error occurred. Please contact the administrator.",
+                request.getDescription(false));
+        
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
 }
