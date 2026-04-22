88cde3f01b2da1b3f3135ee7fff6054d9d7d4d4f
señorMesa <sergioperez.iecc@gmail.com>
2026-04-22 13:08:06 +0100

    chore: create service layer implements UserDetailsService for loading the users

---
+++ src/main/java/com/nosmoke/nexus_ai/service/UserService.java
@@ -0,0 +1,26 @@
+package com.nosmoke.nexus_ai.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nosmoke.nexus_ai.exception.ResourceNotFound;
+import com.nosmoke.nexus_ai.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() ->  new UsernameNotFoundException("User: " + username + " not found"));
+
+        
+    }
+    
+}
