d058c234d90e8a7b0399ed2095a8e83db39561c5
señorMesa <sergioperez.iecc@gmail.com>
2026-04-23 20:05:09 +0100

    feat: added custom validation filter to security configuration for tokens

--- src/main/java/com/nosmoke/nexus_ai/config/JwtFilter.java
+++ src/main/java/com/nosmoke/nexus_ai/config/JwtFilter.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.nosmoke.nexus_ai.utility.JwtUtil;
@@ -30,6 +31,7 @@ import lombok.RequiredArgsConstructor;
  * 
  * OncePerRequestFilter ensures this filter runs exactly once per request, even with request forwarding.
  */
+@Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
--- src/main/java/com/nosmoke/nexus_ai/config/SecurityConfig.java
+++ src/main/java/com/nosmoke/nexus_ai/config/SecurityConfig.java
@@ -10,10 +10,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
 
     
     @Bean
@@ -30,6 +36,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
             
             
